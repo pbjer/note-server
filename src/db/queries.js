c31f88feb5292c49151be9b1762e0d10b455df76
pbjer <prenticebjerk@gmail.com>
2020-06-16 06:33:52 -0700

    add postgres queries for application

---
+++ src/db/queries.js
@@ -0,0 +1,40 @@
+const insertNote = `
+  INSERT INTO notes(google_user_id, id, title, body)
+  VALUES($1, $2, $3, $4)
+`;
+
+const getAllNotes = (options) => {
+  const limit = parseInt(options.limit) ? options.limit.toString() : 'ALL'
+  const order = options.order === 'ASC' ? 'ASC' : 'DESC'
+  const start = parseInt(options.start) ? options.start.toString() : '0'
+  return `
+    SELECT * FROM notes WHERE google_user_id = $1
+      ORDER BY notes.created_at ${order}
+      LIMIT ${limit}
+      OFFSET ${start}
+  `;
+};
+
+const getOneNote = `
+  SELECT * FROM notes
+  WHERE google_user_id = $1 AND id = $2
+`;
+
+const updateNote = `
+  UPDATE notes
+  SET title = $1, body = $2
+  WHERE google_user_id = $3 AND id = $4
+`;
+
+const deleteNote = `
+  DELETE FROM notes
+  WHERE id = $1
+`;
+
+export {
+  insertNote,
+  getAllNotes,
+  getOneNote,
+  updateNote,
+  deleteNote
+};
