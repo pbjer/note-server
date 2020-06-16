a211c77e506a6c3fc6f7c3b11f6cd25d8e4ea550
pbjer <prenticebjerk@gmail.com>
2020-06-16 07:57:37 -0700

    add app entry point, mount middlewares, mount routes

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import notes from './routes/notes';
+import auth from './routes/auth';
+
+const app = express();
+
+// middlewares
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use(cors({
+  origin: [`${process.env.CLIENT_URL}`],
+  credentials: true,
+}));
+app.use(express.json());
+
+// mount routes
+app.use('/user', notes);
+app.use('/auth', auth);
+
+// start the server
+app.listen( port, () => {
+  console.log(`server started at http://localhost:${process.env.SERVER_PORT}`);
+});
