47d02b1003b96df11d15d2c40166d10155770b44
pbjer <prenticebjerk@gmail.com>
2020-06-16 07:50:17 -0700

    add auth route for google, set cookie with signed JWT on successful auth

---
+++ src/routes/auth.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import '../utils/auth';
+
+const auth = express.Router();
+
+auth.get('/google',
+  passport.authenticate('google',{ scope:[
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
+  ]}));
+
+auth.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    const expiration = process.env.COOKIE_EXP;
+    const id = req.user.id;
+    const token = jwt.sign(
+      { id },
+      `${process.env.TOKEN_SECRET}`,
+      { expiresIn: `${process.env.TOKEN_EXP}`
+    });
+
+    res.cookie('token', token, {
+      expires: new Date(Date.now() + expiration),
+      secure: false,
+      httpOnly: false,
+    })
+    res.redirect(`${process.env.CLIENT_URL}/auth-callback`);
+  });
+
+export default auth;
